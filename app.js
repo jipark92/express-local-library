cb9f421cdc0780a41d200e2408e18463081ad990
ji <jiyongprk@gmail.com>
2022-04-22 15:18:43 -0400

    add routes and controller

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
 
 var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://<username>:<123123As>@cluster0.xgnh8.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://OdinUser:OdinUser1!@cluster0.xgnh8.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
