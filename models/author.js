72648479c04de18099e4a1984a86d08a9794ec8f
ji <jiyongprk@gmail.com>
2022-05-04 12:04:59 -0400

    add luxon date format for author list&dtail

--- models/author.js
+++ models/author.js
@@ -1,3 +1,5 @@
+const { DateTime } = require("luxon");
+
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
@@ -46,6 +48,16 @@ AuthorSchema
 .get(function () {
   return '/catalog/author/' + this._id;
 });
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function () {
+    return DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+})
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function () {
+    return DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+})
 
 //Export model
 module.exports = mongoose.model('Author', AuthorSchema);
